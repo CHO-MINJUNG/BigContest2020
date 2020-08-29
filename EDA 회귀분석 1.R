35aab1a87006c468449c0df172da95c7eb609709
chomjung <alswjd420663@gmail.com>
2020-08-30 02:24:49 +0900

    회귀분석 2

--- EDA 회귀분석 1.R
+++ EDA 회귀분석 1.R
@@ -52,7 +52,7 @@ pitcher_stepwise <- function (pitcherdata){
 }
 
 
-teamhitter = read.csv('C:/Users/chomjung/OneDrive - 명지대학교/빅콘테스트2020/데이터 전처리/팀타자 최종.csv', header = TRUE)
+teamhitter = read.csv('C:/Users/chomjung/OneDrive - 명지대학교/빅콘테스트2020/데이터 전처리/1차_최종_데이터/1차_최종_팀타자.csv', header = TRUE)
 teampitcher <- fread('C:/Users/chomjung/OneDrive - 명지대학교/빅콘테스트2020/데이터 전처리/1차_최종_데이터/1차_최종_팀투수.csv', header = TRUE, encoding = 'UTF-8')
 result = pitcher_stepwise(teampitcher)
 
